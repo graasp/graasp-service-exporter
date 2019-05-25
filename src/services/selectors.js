a8758aa1f8b7301d906c169d1c420ae54b9c6a43
Kim <pyphilia@gmail.com>
2019-05-26 00:42:06 +0200

    refactor(export): fix phase paragraph, handle faulty offline labs
    refactor with try/catch statements

--- src/services/selectors.js
+++ src/services/selectors.js
@@ -1,31 +1,34 @@
-const AUDIO_ELEMENTS = 'app-subpage-resource audio';
+const AUDIOS = '//app-subpage-resource/div[audio]';
+const APP_ELEMENTS =
+  'app-graasp-app-resource:not([data-offline-support]) iframe';
 const BASE = 'base';
 const EMBEDDED_ELEMENTS = '.embedded-html';
 const GADGETS = 'div.gadget';
 const HEADER = 'div.header';
-const IMAGES = 'img';
-const INTRODUCTION = '.description p';
-const LAB_ELEMENTS = '//*[not(@data-offline-support)]/iframe'; // use xpath to obtain iframes NOT supporting data-offline support
+const INTRODUCTION = '.header + .description';
+const LAB_ELEMENTS = 'app-gateway-resource:not([data-offline-support]) iframe'; // css :not() doesn't work well
 const OBJECT_ELEMENTS = 'app-subpage-resource object';
-const OFFLINE_READY_IFRAMES = '//*[@data-offline-support]/iframe'; // use xpath to obtain app-gateway-resource and app-app-graasp-app-resource iframes
+const OFFLINE_READY_IFRAMES =
+  'app-gateway-resource[data-offline-support] iframe, app-graasp-app-resource[data-offline-support] iframe'; // use xpath to obtain app-gateway-resource and app-graasp-app-resource iframes
 const META_DOWNLOAD = 'meta[name=download]';
 const PASSWORD = '#password';
-const PHASE_DESCRIPTIONS = 'description';
-const PHASE_TITLES = 'name';
-const RESOURCES = 'resources';
+const PHASE_DESCRIPTIONS = 'section > .description';
+const PHASE_TITLES = '.name';
+const RESOURCES = '.resources';
 const SPACE_TITLE = 'div.header > h1';
-const SUBPAGES = '.export > section';
+const SUBPAGES = 'app-subpage-content.export';
 const TOOLS = '.tools > section';
-const UNSUPPORTED_ELEMENTS = '.resources .unsupported';
+const UNSUPPORTED_ELEMENTS = '.unsupported';
 const USERNAME = '#username';
+const VIDEOS = 'video';
 
 export {
-  AUDIO_ELEMENTS,
+  AUDIOS,
+  APP_ELEMENTS,
   BASE,
   EMBEDDED_ELEMENTS,
   GADGETS,
   HEADER,
-  IMAGES,
   INTRODUCTION,
   LAB_ELEMENTS,
   META_DOWNLOAD,
@@ -40,4 +43,5 @@ export {
   TOOLS,
   UNSUPPORTED_ELEMENTS,
   USERNAME,
+  VIDEOS,
 };
